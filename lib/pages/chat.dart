7a4e7c5d1a8018269bdc7dbc5e305eafc686a421
Princen <c14200181@john.petra.ac.id>
2023-05-29 15:23:59 +0800

    Update chat.dart

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -19,78 +19,88 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Chat',
-          style: TextStyle(color: Color.fromARGB(255, 47, 42, 42)),
+    final user = _auth.currentUser;
+    final messagesCollection = _firestore.collection('messages'); // Reference the 'messages' collection directly
+
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Chat',
+            style: TextStyle(color: Color.fromARGB(255, 47, 42, 42)),
           ),
-        backgroundColor:  const Color.fromARGB(255, 192, 192, 12),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: _firestore
-                  .collection('messages')
-                  .orderBy('timestamp')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const CircularProgressIndicator();
-                }
+          backgroundColor: const Color.fromARGB(255, 192, 192, 12),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: messagesCollection.orderBy('timestamp').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
 
-                final messages = snapshot.data!.docs.reversed.toList();
+                  final messages = snapshot.data!.docs.reversed.toList();
 
-                return ListView.builder(
-                  reverse: true,
-                  itemCount: messages.length,
-                  itemBuilder: (context, index) {
-                    final message = messages[index].get('text');
-                    return ListTile(
-                      title: Text(message),
-                    );
-                  },
-                );
-              },
+                  return ListView.builder(
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final message = messages[index].get('text');
+                      final sender = messages[index].get('sender');
+                      return ListTile(
+                        title: Text(message),
+                        subtitle: Text('Sent by: $sender'),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _messageController,
-                    decoration: const InputDecoration(
-                      hintText: 'Type your message...',
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: const InputDecoration(
+                        hintText: 'Type your message...',
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () {
-                    final user = _auth.currentUser;
-                    if (user != null) {
-                      _firestore.collection('messages').add({
-                        'text': _messageController.text,
-                        'sender': user.email,
-                        'timestamp': FieldValue.serverTimestamp(),
-                      });
-                      _messageController.clear();
-                    }
-                  },
-                ),
-              ],
+                  IconButton(
+                    icon: const Icon(Icons.send),
+                    onPressed: () {
+                      if (user != null) {
+                        messagesCollection.add({
+                          'text': _messageController.text,
+                          'sender': user.email,
+                          'timestamp': FieldValue.serverTimestamp(),
+                        });
+                        _messageController.clear();
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: buildBottomNavBar(
-        _currentIndex,
-        MediaQuery.of(context).size,
-        false,
-      ),
-    );
+          ],
+        ),
+        bottomNavigationBar: buildBottomNavBar(
+          _currentIndex,
+          MediaQuery.of(context).size,
+          false,
+        ),
+      );
+    } else {
+      // Handle case where user is not authenticated
+      return const Scaffold(
+        body: Center(
+          child: Text('User not authenticated'),
+        ),
+      );
+    }
   }
 }
